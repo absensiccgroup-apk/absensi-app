2531d9541d7d0ed97c1b887fd88246ab3bee92ae
MiningRoom51 <miningroom51@gmail.com>
2026-06-06 18:24:56 +0700

    Updates

--- sw.js
+++ sw.js
@@ -1,25 +1,9 @@
-const CACHE = 'absensi-v2';
-const URLS  = ['/absensi-app/'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  // Network first untuk API, cache first untuk assets
-  if (e.request.url.includes('supabase') || e.request.url.includes('script.google')) {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
-  }
+// Service worker ini akan unregister dirinya sendiri
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.registration.unregister())
+  );
 });
